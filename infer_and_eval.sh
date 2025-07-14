36356eb0af998c8bca0e0313c7fc2f4506afe94e
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-07-14 23:50:58 +0900

    vllm context length

--- infer_and_eval.sh
+++ infer_and_eval.sh
@@ -13,9 +13,12 @@ eval_jsonl=${model_dir}/${target_jsonl}/completion.jsonl
 if [[ $model_dir == *"Llama-3"* ]]; then
     python -m llmpp.completion_vllm --m ${model_dir} --i ${target_jsonl} \
         --ct '^\{\{- bos_token \}\}\n' ${vllm_options}
-elif [[ $model_dir == *"gemma-2"* ]] || [[ $model_dir == *"Llama-2"* ]] || [[ $model_dir == *"llm-jp-"* ]] || [[ $model_dir == *"OLMo-"* ]]; then
+elif [[ $model_dir == *"gemma-2"* ]]; then
     python -m llmpp.completion_vllm --m ${model_dir} --i ${target_jsonl} \
         --rsr ${vllm_options}
+elif [[ $model_dir == *"Llama-2"* ]] || [[ $model_dir == *"llm-jp-"* ]] || [[ $model_dir == *"OLMo-"* ]]; then
+    python -m llmpp.completion_vllm --m ${model_dir} --i ${target_jsonl} \
+        --rsr --max_tokens 4096 ${vllm_options}
 else
     python -m llmpp.completion_vllm --m ${model_dir} --i ${target_jsonl} \
         ${vllm_options}

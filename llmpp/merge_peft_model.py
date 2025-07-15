524b5ac2ecd6bed03f0ab3d42dfdb961bf87abff
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-07-15 17:24:30 +0900

    merge_peft_model.py

---
+++ llmpp/merge_peft_model.py
@@ -0,0 +1,36 @@
+import sys
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftConfig, PeftModel
+
+
+def main():
+    peft_model = sys.argv[1]
+    peft_config = PeftConfig.from_pretrained(peft_model)
+    base_model = peft_config.base_model_name_or_path
+    output_dir = peft_model.rstrip("/") + ".merged"
+    print(f"merging {peft_model=} into {base_model=}", sys.stderr)
+    save_merged_weights(
+        base_model_path=base_model,
+        peft_model_path=peft_model,
+        output_dir=output_dir,
+    )
+    print(f"merge completed and merged model saved in {output_dir}", sys.stderr)
+
+
+def save_merged_weights(
+    base_model_path,
+    peft_model_path,
+    output_dir,
+):
+    model = AutoModelForCausalLM.from_pretrained(base_model_path, torch_dtype=torch.bfloat16, device_map="auto")
+    model = PeftModel.from_pretrained(model, peft_model_path)
+    merged_model = model.merge_and_unload()
+    merged_model.save_pretrained(output_dir)
+    tokenizer = AutoTokenizer.from_pretrained(peft_model_path)
+    tokenizer.save_pretrained(output_dir)
+
+
+if __name__ == "__main__":
+    main()

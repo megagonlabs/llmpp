a1f50baec95b632119102539d3ea88447ca028a1
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-07-17 01:24:11 +0900

    sharding when merging

--- llmpp/merge_peft_model.py
+++ llmpp/merge_peft_model.py
@@ -27,7 +27,7 @@ def save_merged_weights(
     model = AutoModelForCausalLM.from_pretrained(base_model_path, torch_dtype=torch.bfloat16, device_map="auto")
     model = PeftModel.from_pretrained(model, peft_model_path)
     merged_model = model.merge_and_unload()
-    merged_model.save_pretrained(output_dir)
+    merged_model.save_pretrained(output_dir, max_shard_size="2GB", safe_serialization=True)
     tokenizer = AutoTokenizer.from_pretrained(peft_model_path)
     tokenizer.save_pretrained(output_dir)
 

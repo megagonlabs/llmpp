21da5aa360cd010539754ef43e0388264e8e6e6b
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-08-14 16:59:21 +0900

    add --b option to sft_infer_eval.sh

--- sft_infer_eval.sh
+++ sft_infer_eval.sh
@@ -6,10 +6,15 @@ sft_method=llmpp.sft_$1
 model=$2
 dataset=$3
 templates=${@:4:($#-3)}
+batch_size=""
 
 for template in ${templates}
 do
-  python -m ${sft_method} \
+  if [[ "${template}" == "--b" ]]; then
+    batch_size="--b ${template}"
+    continue
+  fi
+  python -m ${sft_method} ${batch_size} \
     --c config/${model}.yaml \
     --t data/${dataset}/${template}.train.jsonl
 

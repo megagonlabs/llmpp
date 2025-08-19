0ccc1555bfcb424eca81fcd7e5872c04e91fe88c
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-08-19 09:39:06 +0900

    improve

--- llmpp/sampling.py
+++ llmpp/sampling.py
@@ -4,6 +4,7 @@ import sys
 
 def main():
     prob = float(sys.argv[1])
+    random.seed(int(sys.argv[2]) if len(sys.argv) > 2 else 42)
     for _ in sys.stdin:
         if random.random() <= prob:
             print(_, end="")

e09a874bc84f32a1f8e704722d9743fd0ca18419
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-08-20 16:54:43 +0900

    --lr

--- llmpp/sft_base.py
+++ llmpp/sft_base.py
@@ -27,7 +27,7 @@ def get_config(parser: ArgumentParser = None):
     parser.add_argument("--gradient_accumulation_steps", "--ga", type=int)
     parser.add_argument("--max_seq_length", "--l", type=int)
     parser.add_argument("--num_train_epochs", "--e", type=int)
-    parser.add_argument("--learning_rate", "--lr", type=float)
+    parser.add_argument("--learning_rate", "--lr")
     parser.add_argument("--load_in_4bit", "--li4", action="store_true")
     parser.add_argument("--load_in_8bit", "--li8", action="store_true")
     parser.add_argument("--lora_r", "--r", type=int)
@@ -56,7 +56,7 @@ def get_config(parser: ArgumentParser = None):
     if args.num_train_epochs:
         config["sft_config_args"]["num_train_epochs"] = args.num_train_epochs
     if args.learning_rate:
-        config["sft_config_args"]["learning_rate"] = args.learning_rate
+        config["sft_config_args"]["learning_rate"] = float(args.learning_rate)
     if args.load_in_4bit:
         config["model_args"]["load_in_4bit"] = True
     if args.load_in_8bit:

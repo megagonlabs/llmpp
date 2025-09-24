8bc36ac6bf7de73824d9d4bc0ba2972b6eb35ac6
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-09-24 16:52:43 +0900

    wait

--- conllu_to_prompt.sh
+++ conllu_to_prompt.sh
@@ -7,3 +7,4 @@ l=$2
 python -m llmpp.conllu_to_prompt --t templates/en-*.toml                --i ${d}/*.conllu --l ${l} &
 python -m llmpp.conllu_to_prompt --t templates/en-linearized-step*.toml --i ${d}/*.conllu --l ${l} --s sa  --r " " &
 python -m llmpp.conllu_to_prompt --t templates/en-linearized-step*.toml --i ${d}/*.conllu --l ${l} --s sa2 --r "  " &
+wait

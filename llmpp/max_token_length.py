47d52c6a2e5d225890e1089a4ae459090f3dad9c
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-10-05 12:29:17 +0900

    debug

--- llmpp/max_token_length.py
+++ llmpp/max_token_length.py
@@ -39,9 +39,10 @@ def main():
             else:
                 input_path = jsonl_path
             with open(input_path, "r", encoding="utf8") as fin:
-                for line_index, line in enumerate(fin, 1):
+                for line_index, line in enumerate(fin.readlines(), 1):
+                    line = line.rstrip("\n")
                     if input_path.endswith(".text"):
-                        text = line.rstrip("\n")
+                        text = line
                         char_length = len(text)
                         token_length = len(tokenizer.encode(text))
                     else:
@@ -52,7 +53,7 @@ def main():
                     local_total_char += char_length
                     local_total_token += token_length
                     if token_length <= max_length:
-                        print(line, end="", file=sys.stderr)
+                        print(line, file=sys.stderr)
                     elif max_length > 0:
                         print("removed line", line_index, token_length)
                     local_max = sorted(local_max + [token_length], reverse=True)[:LOCAL_TOP_N]

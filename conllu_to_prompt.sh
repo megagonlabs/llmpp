1edb25f24cde285bb6c372538e0afe26edb40f56
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-10-12 23:01:37 +0900

    debug

--- conllu_to_prompt.sh
+++ conllu_to_prompt.sh
@@ -5,6 +5,6 @@ set -eu
 d=$1
 l=$2
 for t in `ls templates/*.toml` ; do
-  python -m llmpp.conllu_to_prompt --t t --i ${d}/*.conllu --l ${l} &
+  python -m llmpp.conllu_to_prompt --t ${t} --i ${d}/*.conllu --l ${l} &
 done
 wait

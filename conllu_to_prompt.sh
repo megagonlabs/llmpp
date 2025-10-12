bb19767abb5db852bbcb7c471b296b398bd76a27
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-10-13 06:42:56 +0900

    improve

--- conllu_to_prompt.sh
+++ conllu_to_prompt.sh
@@ -2,9 +2,11 @@
 
 set -eu
 
-d=$1
-l=$2
-for t in `ls templates/*.toml` ; do
-  python -m llmpp.conllu_to_prompt --t ${t} --i ${d}/*.conllu --l ${l} &
+for d in $@ ; do
+  lang=${d#data/}
+  lang=${lang%:*}
+  for t in `ls templates/*.toml` ; do
+    python -m llmpp.conllu_to_prompt --t ${t} --i ${d}/*.conllu --l ${lang} &
+  done
 done
 wait
--- download_ud.sh
+++ download_ud.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 LONG_NAME=$1  # like UD_Japanese-GSD
 SHORT_NAME=$2  # like ja_gsd
 REVISION=$3  # like r2.15
-OUTPUT_DIR=$4
-if [ -z "$OUTPUT_DIR" ]; then  # like ./data/ja_gsd-r2.15/
-    OUTPUT_DIR=./data/$SHORT_NAME-$REVISION
+if [ $# -ge 4 ] ; then
+    OUTPUT_DIR=$4
+else
+    LANGUAGE=${LONG_NAME#UD_}
+    LANGUAGE=${LANGUAGE%-*}
+    OUTPUT_DIR=./data/${LANGUAGE}:${SHORT_NAME}-${REVISION}
 fi
 
-mkdir -p $OUTPUT_DIR
+mkdir -p ${OUTPUT_DIR}
 
 for SET in train dev test ; do
-    FILE_NAME=$SHORT_NAME-ud-$SET.conllu
-    curl -o $OUTPUT_DIR/$FILE_NAME https://raw.githubusercontent.com/UniversalDependencies/$LONG_NAME/refs/tags/$REVISION/$FILE_NAME
+    FILE_NAME=${SHORT_NAME}-ud-${SET}.conllu
+    curl -o ${OUTPUT_DIR}/${FILE_NAME} https://raw.githubusercontent.com/UniversalDependencies/${LONG_NAME}/refs/tags/${REVISION}/${FILE_NAME}
 done

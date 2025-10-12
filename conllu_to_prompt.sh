f8bcd028f40f173f91d075ba01bc212afff7b5d7
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-10-12 15:35:44 +0900

    improve conllu_to_prompt.sh

--- conllu_to_prompt.sh
+++ conllu_to_prompt.sh
@@ -4,7 +4,7 @@ set -eu
 
 d=$1
 l=$2
-python -m llmpp.conllu_to_prompt --t templates/en-*.toml                --i ${d}/*.conllu --l ${l} &
-python -m llmpp.conllu_to_prompt --t templates/en-linearized-step*.toml --i ${d}/*.conllu --l ${l} --s sa  --r " " &
-python -m llmpp.conllu_to_prompt --t templates/en-linearized-step*.toml --i ${d}/*.conllu --l ${l} --s sa2 --r "  " &
+for t in `ls templates/*.toml` ; do
+  python -m llmpp.conllu_to_prompt --t t --i ${d}/*.conllu --l ${l} &
+done
 wait

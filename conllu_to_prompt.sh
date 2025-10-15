2897f68dc398f6f9fe28624516bc65011082cea7
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-10-16 01:53:52 +0900

    template_path

--- conllu_to_prompt.sh
+++ conllu_to_prompt.sh
@@ -2,10 +2,11 @@
 
 set -eu
 
-for d in $@ ; do
+template_path=$1
+for d in ${@:2:($#-1)} ; do
   lang=${d#data/}
-  lang=${lang%:*}
-  for t in `ls templates/*.toml` ; do
+  lang=${lang%@*}
+  for t in `ls ${template_path}` ; do
     python -m llmpp.conllu_to_prompt --t ${t} --i ${d}/*.conllu --l ${lang} &
   done
 done

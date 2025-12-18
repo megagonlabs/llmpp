1ce3e508f6ae4bf2999a526a4bb74566f7b59526
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-12-18 11:09:04 +0900

    no content

--- llmpp/eval.py
+++ llmpp/eval.py
@@ -58,8 +58,11 @@ def main():
             with open(output_eval_json_path, "w", encoding="utf8") as f_eval:
                 json.dump(stats, f_eval, ensure_ascii=False, indent=1)
                 print(file=f_eval)
-            print(completion_results_jsonl, stats["digest"], file=sys.stderr)
-            print(completion_results_jsonl, *[stats[u][k] for u in REPORTING_FIELDS for k in REPORTING_FIELDS[u]], sep="\t")
+            if stats["sentence"]["gold"] > 0:
+                print(completion_results_jsonl, stats["digest"], file=sys.stderr)
+                print(completion_results_jsonl, *[stats[u][k] for u in REPORTING_FIELDS for k in REPORTING_FIELDS[u]], sep="\t")
+            else:
+                print("no content", completion_results_jsonl, file=sys.stderr)
 
             stats = eval(completion_results, args.index_field, args.use_deprel_subtypes, None, ignore_punct=True)
             stats["config"] = config
--- llmpp/eval_tokenize.py
+++ llmpp/eval_tokenize.py
@@ -36,8 +36,11 @@ def main():
             with open(output_eval_json_path, "w", encoding="utf8") as f_eval:
                 json.dump(stats, f_eval, ensure_ascii=False, indent=1)
                 print(file=f_eval)
-            print(completion_results_jsonl, stats["digest"], file=sys.stderr)
-            print(completion_results_jsonl, *[stats[u][k] for u in REPORTING_FIELDS for k in REPORTING_FIELDS[u]], sep="\t")
+            if stats["sentence"]["gold"] > 0:
+                print(completion_results_jsonl, stats["digest"], file=sys.stderr)
+                print(completion_results_jsonl, *[stats[u][k] for u in REPORTING_FIELDS for k in REPORTING_FIELDS[u]], sep="\t")
+            else:
+                print("no content", completion_results_jsonl, file=sys.stderr)
         except Exception as e:
             print("skipping", completion_results_jsonl, file=sys.stderr)
             if args.stop_on_error:

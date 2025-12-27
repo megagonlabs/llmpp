0e1e8ce7d949ca3ee8044d3fdd8c13c9afafd022
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-12-28 00:57:01 +0900

    improve

--- llmpp/dispatch_lines_by_pattern.py
+++ llmpp/dispatch_lines_by_pattern.py
@@ -6,22 +6,22 @@ from collections import defaultdict
 
 def main():
     language_pattern = re.compile(sys.argv[1])
-    output_path_base = sys.argv[2]
-    input_files = sys.argv[3:]
+    input_files = sys.argv[2:]
     language_lines = defaultdict(list)
     for input_file in input_files:
+        print("start", input_file, file=sys.stderr)
         with open(input_file, "r", encoding="utf8") as fin:
-            for line in fin.readlines():
+            for idx, line in enumerate(fin.readlines(), 1):
                 m = language_pattern.search(line)
                 for g in m.groups() if m else []:
                     if g:
                         language_lines[g].append(line)
                         break
                 else:
-                    print("no match:", line, end="", file=sys.stderr)
-    for language, lines in language_lines.items():
-        with open(f"{output_path_base}.{language}", "w", encoding="utf8") as fout:
-            print(*lines, sep="", end="", file=fout)
+                    print(f"no match: {input_file} #{idx},", line, end="", file=sys.stderr)
+        for language, lines in language_lines.items():
+            with open(f"{input_file}.{language}", "w", encoding="utf8") as fout:
+                print(*lines, sep="", end="", file=fout)
 
 
 if __name__ == "__main__":

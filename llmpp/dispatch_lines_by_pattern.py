f3a4d6abb10c9f01050e77eb0c642b988662fc46
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-12-27 23:59:51 +0900

    dispatch_lines_by_pattern.py

---
+++ llmpp/dispatch_lines_by_pattern.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+from collections import defaultdict
+
+
+def main():
+    language_pattern = re.compile(sys.argv[1])
+    output_path_base = sys.argv[2]
+    input_files = sys.argv[3:]
+    language_lines = defaultdict(list)
+    for input_file in input_files:
+        with open(input_file, "r", encoding="utf8") as fin:
+            for line in fin.readlines():
+                m = language_pattern.search(line)
+                if m and len(m.groups()):
+                    language = m.group(1)
+                    language_lines[language].append(line)
+    for language, lines in language_lines.items():
+        with open(f"{output_path_base}.{language}", "w", encoding="utf8") as fout:
+            print(*lines, sep="", file=fout)
+
+
+if __name__ == "__main__":
+    main()

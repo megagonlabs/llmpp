884cea1b2c7bbc42e89bc0aed89c9427bdf6149f
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2026-02-02 14:59:00 +0900

    debug

--- llmpp/conllu_to_docs.py
+++ llmpp/conllu_to_docs.py
@@ -14,7 +14,7 @@ def main():
     with open(template_path, "rb") as fin:
         template = tomllib.load(fin)
     for conllu_path in conllu_path_list:
-        print("processing", conllu_path, file=sys.stederr)
+        print("processing", conllu_path, file=sys.stderr)
         random.seed(42)
         m = UD_PATH_PATTERN.match(conllu_path) or DATASET_PATH_PATTERN.match(conllu_path)
         lang = m.group(1)

7932940fccb1bea3a9a25ef13b9b0b60b56aceec
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2026-04-22 11:39:15 +0900

    add HEAD metrics

--- llmpp/aggregate_confusion_matrices.py
+++ llmpp/aggregate_confusion_matrices.py
@@ -9,7 +9,7 @@ model_path_pattern = re.compile(r"/(.+?)[-_]([0-9]+)[Bb]-stage([0-9]+)(?:-ingred
 
 upos_list = ["ADJ", "ADP", "ADV", "AUX", "CCONJ", "DET", "INTJ", "NOUN", "NUM", "PART", "PRON", "PROPN", "PUNCT", "SCONJ", "SYM", "VERB", "X"]
 
-deprel_list = ["acl", "advcl", "advmod", "amod", "appos", "aux", "case", "cc", "ccomp", "clf", "compound", "conj", "cop", "csubj", "det", "dep", "discourse", "dislocated", "expl", "fixed", "flat", "goeswith", "iobj", "list", "mark", "nmod", "nsubj", "nummod", "obj", "obl", "orphan", "parataxis", "punct", "reparandum", "root", "vocative", "xcomp", "-"]
+deprel_list = ["acl", "advcl", "advmod", "amod", "appos", "aux", "case", "cc", "ccomp", "clf", "compound", "conj", "cop", "csubj", "det", "dep", "discourse", "dislocated", "expl", "fixed", "flat", "goeswith", "iobj", "list", "mark", "nmod", "nsubj", "nummod", "obj", "obl", "orphan", "parataxis", "punct", "reparandum", "root", "vocative", "xcomp"]
 
 
 def calc_stats(stats, keys=None, key_pattern=None):
@@ -54,12 +54,55 @@ def calc_stats(stats, keys=None, key_pattern=None):
     }
 
 
+def calc_head_stats(stats, keys=None, key_pattern=None):
+    gold_total = 0
+    content_total = 0
+    correct_total = 0
+    gold_stats = defaultdict(int)
+    content_stats = defaultdict(int)
+    correct_stats = defaultdict(int)
+    if keys:
+        gold_keys = sorted(keys)
+    elif key_pattern:
+        gold_keys = sorted({re.search(key_pattern, k).group(1) for k in stats})
+    else:
+        gold_keys = sorted(stats.keys())
+    for gold, stat in stats.items():
+        if key_pattern:
+            gold = re.search(key_pattern, gold).group(1)
+        for content, c in stat.items():
+            if key_pattern:
+                content = re.search(key_pattern, content).group(1)
+            gold_total += c
+            gold_stats[gold] += c
+            if content != "null":
+                content_total += c
+                content_stats[gold] += c
+                if content == "-":
+                    correct_total += c
+                    correct_stats[gold] += c
+    recalls = {"*": correct_total / gold_total if gold_total else 0.}
+    for key in gold_keys:
+        recalls[key] = correct_stats.get(key, 0) / gold_stats[key] if gold_stats.get(key) else 0.
+    precisions = {"*": correct_total / content_total if content_total else 0.}
+    f1s = {}
+    for key in gold_keys:
+        precisions[key] = correct_stats.get(key, 0) / content_stats[key] if content_stats.get(key) else 0.
+        f1s[key] = f1(recalls[key], precisions[key])
+    return {
+        "recall": recalls,
+        "precision": precisions,
+        "f1": f1s,
+    }
+
+
 def f1(r, p):
     return 2. * r * p / (r + p) if r * p else 0.
 
 def main():
     upos_stats = {}
     deprel_stats = {}
+    head_stats = {}
     total_stats = {}
     for target_path in sys.argv[1:]:
         m = model_path_pattern.search(target_path)
@@ -80,6 +123,10 @@ def main():
         r = deprel_stats[key]["recall"]["*"] = result["token"]["correct_head_deprel"] / (result["token"]["gold"] or 1)
         p = deprel_stats[key]["precision"]["*"] = result["token"]["correct_head_deprel"] / (result["token"]["content"] or 1)
         deprel_stats[key]["f1"]["*"] = f1(r, p)
+        head_stats[key] = calc_stats(calc_head_stats(result["confusion_deprel"], keys=deprel_list, key_pattern=r"^([^:]+)")
+        r = head_stats[key]["recall"]["*"] = result["token"]["correct_head"] / (result["token"]["gold"] or 1)
+        p = head_stats[key]["precision"]["*"] = result["token"]["correct_head"] / (result["token"]["content"] or 1)
+        head_stats[key]["f1"]["*"] = f1(r, p)
         total_stats[key] = {}
         total_stats[key]["recall"] = {
             "Aligned": result["token"]["aligned"] / (result["token"]["gold"] or 1),
@@ -100,6 +147,9 @@ def main():
         ["DEPREL Recall", deprel_stats, "recall"],
         ["DEPREL Precision", deprel_stats, "precision"],
         ["DEPREL F1", deprel_stats, "f1"],
+        ["HEAD Recall", head_stats, "recall"],
+        ["HEAD Precision", head_stats, "precision"],
+        ["HEAD F1", head_stats, "f1"],
         ["TOTAL Recall", total_stats, "recall"],
         ["TOTAL Precision", total_stats, "precision"],
         ["TOTAL F1", total_stats, "f1"],
